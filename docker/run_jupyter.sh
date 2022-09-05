80a3af5e8f7531545ead3c66ede39a6dca97bf61
Dominik Stiller <domi.stiller@gmail.com>
2022-09-05 13:10:07 +0200

    Update Jupyter run script

--- docker/run_jupyter.sh
+++ docker/run_jupyter.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 nvidia-docker run -it \
 	-p 8888:8888 \
-	-v $(pwd)/..:/work \
+	-v ~/dev/tudelft-assignments/DTU02456:/work \
    dominikstiller/pytorch:gpu \
-	jupyter notebook --ip=0.0.0.0 --allow-root
+	jupyter lab --ip=0.0.0.0 --allow-root
